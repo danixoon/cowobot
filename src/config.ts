dd8f62dcd1daee1fbaf28e1fb90fefa1ed40c239
Daniil Bombenkov <danixoon@yandex-team.ru>
2020-04-18 12:54:04 +0300

    project structure

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+import * as dotenv from "dotenv";
+
+let isLoaded = false;
+
+export const loadConfig = (type: "production" | "development") => {
+  if (isLoaded) throw new Error("Config already loaded");
+  const config = dotenv.config({ path: `/config/.env.${type}` });
+  if (config.error) throw config.error;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import * as express from "express";
+import { loadConfig } from "./config";
+
+loadConfig("development");
+
+const { PORT } = process.env;
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("dat is works!");
+  res.send("Dat is works!");
 });
 
-app.listen(3000, () => {
-  console.log("server is listening");
+app.listen(PORT, () => {
+  console.log("Server is listening on port " + PORT);
 });
