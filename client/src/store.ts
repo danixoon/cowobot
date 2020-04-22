64a9278225e912cbc6abee0e49711ffd9197665d
Bombenkov Daniil <danixoon@yandex-team.ru>
2020-04-23 00:37:42 +0300

    create-react-app ejected & ts types WIP

---
+++ client/src/store.ts
@@ -0,0 +1,10 @@
+/ <reference path="./types/index.d.ts" />
+import { createStore } from "redux";
+
+// import { IAction } from "./index";
+// import * from "";
+
+export const rootReducer = (state, action) => {
+  return state;
+};
+// export const store = createStore();
