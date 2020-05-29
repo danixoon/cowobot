66d0de7559694dd68838a5187a0fea577a9da794
Bombenkov Daniil <danixoon@yandex-team.ru>
2020-05-29 15:11:02 +0300

    removed .gitignore cached files

--- build/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var router = express.Router();
-router.get("/test", function (req, res, next) {
-    res.send({ ok: true });
-    next();
-});
-exports.default = router;
